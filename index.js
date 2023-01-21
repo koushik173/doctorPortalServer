39a78d860217464496dc7a0e87a10cd38c190f60
koushik173 <cse_2012020173@lus.ac.bd>
2023-01-22 02:40:36 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express()
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = "mongodb+srv://doctor_admin:<password>@cluster0.sr446qq.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+app.get('/', (req, res) => {
+  res.send('Hello from doctor Uncle!')
+})
+
+app.listen(port, () => {
+  console.log(`Doctors app port ${port}`)
+})
